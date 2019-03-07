b18f14fd54c33261dd9addc5e56442191bb071d0
Oguzhan Dogan <dogan_oguzhan@hotmail.com>
2019-03-07 15:49:05 +0300

    Custom property & event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   serverElements = [{type: 'server', name: 'Mars', content: 'This is our development server'},
     {type: 'blueprint', name: 'Jupiter', content: 'This is test server'}];
 
+
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 }
